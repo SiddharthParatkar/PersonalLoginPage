fcb359ffaeccc6afc8e144e156c2d6327cdc4dcb
Siddharth Paratkar <siddharthparatkar@yahoo.com>
2020-07-17 15:52:16 -0400

    changing App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './App.css';
 import { Login } from "./components/login";
 
-function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoginActive: true,
+    }
+  }
+  
+  render(){
+    const { isLoginActive } = this.state;
+    return (
+      <div className="App">
+        <div className="login">
+          <div className="container">
+            {isLoginActive && <Login />}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
