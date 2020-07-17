b98f31541d9185e8fe7e2a16da4fd011e3c06538
Siddharth Paratkar <siddharthparatkar@yahoo.com>
2020-07-17 16:05:03 -0400

    container style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import './App.css';
-import { Login } from "./components/login";
+import React from "react";
+import "./App.css";
+import { Login, Register } from "./components/login/index";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoginActive: true,
-    }
+    };
   }
-  
-  render(){
+
+  changeState() {
     const { isLoginActive } = this.state;
+    
+  }
+
+  render() {
+    const { isLoginActive } = this.state;
+    const current = isLoginActive ? "Register" : "Login";
+    const currentActive = isLoginActive ? "Login" : "Register"
     return (
       <div className="App">
         <div className="login">
           <div className="container">
-            {isLoginActive && <Login />}
+            {isLoginActive && (
+              <Login containerRef={(ref) => (this.current = ref)} />
+            )}
+            {!isLoginActive && (
+              <Register containerRef={(ref) => (this.current = ref)} />
+            )}
           </div>
+          <RightSide current={current} containerRef={ref => this.RightSide = ref} onClick={this.changeState.bind(this)}/>
         </div>
       </div>
-    )
+    );
   }
 }
+
+const RightSide = (props) => {
+  return (
+    <div
+      className="right-side"
+      ref={props.containerRef}
+      onClick={props.onClick}
+    >
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
